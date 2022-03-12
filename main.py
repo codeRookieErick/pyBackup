8f541b89c33e1f86e55d7ff6d7b5351e23e74729
codeRookieErick <erickmoradev@gmail.com>
2022-03-12 19:43:22 -0400

    Making it GPL... i suppose...

---
+++ main.py
@@ -0,0 +1,18 @@
+"""
+Pybackup. A simple zip based version manager. Copyright (c) 2022 Erick Fernando Mora
+
+This program is free software; you can redistribute it and/or modify it under the terms 
+of the GNU General Public License as published by the Free Software Foundation version 3
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with this program; 
+if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, 
+MA 02111-1307 USA
+
+erickfernandomoraramirez@gmail.com
+dev.moradev.dev
+"""
+
